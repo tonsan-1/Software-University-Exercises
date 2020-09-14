344a5adfdf77aba2d79598c9fa101894338e4cdd
tonsan221 <68417157+tonsan221@users.noreply.github.com>
2020-09-14 14:27:28 +0300

    Add files via upload

---
+++ C# Fundamentals/Basic Syntax - Excercises/05.Login/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _05.Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var username = Console.ReadLine();
+            var charArray = username.ToCharArray();
+            Array.Reverse(charArray);
+            var password = new string(charArray);
+            var counter = 0;
+            string input;
+            while (true)
+            {
+                input = Console.ReadLine();
+                if (input != password)
+                {
+                    counter++;
+                    if (counter == 4)
+                    {
+                        Console.WriteLine($"User {username} blocked!");
+                        return;
+                    }
+                    Console.WriteLine("Incorrect password. Try again.");
+                }
+                else
+                {
+                    Console.WriteLine($"User {username} logged in.");
+                    return;
+                }
+            }
+        }
+    }
+}
