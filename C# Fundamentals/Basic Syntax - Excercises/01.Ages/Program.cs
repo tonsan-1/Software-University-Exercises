3d25678abe5bb00a47643a33ed09627fed5ad922
tonsan221 <68417157+tonsan221@users.noreply.github.com>
2020-09-14 14:16:49 +0300

    Add files via upload

---
+++ C# Fundamentals/Basic Syntax - Excercises/01.Ages/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _01.Ages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var age = int.Parse(Console.ReadLine());
+            string person = "";
+
+            if (age <= 2)
+            {
+                person = "baby";
+            }
+            else if (age > 2 && age <= 13)
+            {
+                person = "child";
+            }
+            else if (age > 13 && age <= 19)
+            {
+                person = "teenager";
+            }
+            else if (age > 19 && age <= 65)
+            {
+                person = "adult";
+            }
+            else if (age >= 65)
+            {
+                person = "elder";
+            }
+            else
+            {
+                return;
+            }
+            Console.WriteLine(person);
+        }
+    }
+}
