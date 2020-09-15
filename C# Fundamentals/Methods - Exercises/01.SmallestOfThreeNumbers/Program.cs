255343244d93484603c33830cd21932aac3bda8f
tonsan221 <68417157+tonsan221@users.noreply.github.com>
2020-09-15 13:43:39 +0300

    Add files via upload

---
+++ C# Fundamentals/Methods - Exercises/01.SmallestOfThreeNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01.SmallestOfThreeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = int.Parse(Console.ReadLine());
+            int num2 = int.Parse(Console.ReadLine());
+            int num3 = int.Parse(Console.ReadLine());
+            PrintSmallestNumber(num1, num2, num3);
+        }
+
+        private static void PrintSmallestNumber(int num1, int num2, int num3)
+        {
+            var smallestOfTwo = Math.Min(num1, num2);
+            var smallest = Math.Min(smallestOfTwo, num3);
+            Console.WriteLine(smallest);
+        }
+    }
+}
---
+++ C# Fundamentals/Methods - Exercises/02.VowelsCount/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _02.VowelsCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine().ToLower();
+            char[] letters = input.ToCharArray();
+            char[] vowels = { 'a', 'e', 'o', 'u', 'i' };
+            PrintVowelsCount(letters, vowels);
+        }
+
+        private static void PrintVowelsCount(char[] letters, char[] vowels)
+        {
+            var count = 0;
+            foreach (var vowel in vowels)
+            {
+                foreach (var letter in letters)
+                {
+                    if (letter == vowel)
+                    {
+                        count++;
+                    }
+                }
+            }
+            Console.WriteLine(count);
+        }
+    }
+}
