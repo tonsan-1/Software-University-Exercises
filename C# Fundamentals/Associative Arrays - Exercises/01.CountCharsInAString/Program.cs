67c1d51a17a2c0e02b7cd662e51f755f95495f7c
tonsan221 <68417157+tonsan221@users.noreply.github.com>
2020-09-15 14:20:48 +0300

    Add files via upload

---
+++ C# Fundamentals/Associative Arrays - Exercises/01.CountCharsInAString/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.CountCharsInAString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, uint> histogram = new Dictionary<char, uint>();
+
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                var currChar = text[i];
+
+                if (currChar != ' ')
+                {
+
+                    if (!histogram.ContainsKey(currChar))
+                    {
+                        histogram[currChar] = 0;
+                    }
+
+                    histogram[currChar]++;
+                }
+            }
+
+            foreach (var kvp in histogram)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
---
+++ C# Fundamentals/Associative Arrays - Exercises/02.AMinerTask/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02.AMinerTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var materials = new Dictionary<string, long>();
+
+            string product;
+            while ((product = Console.ReadLine()) != "stop")
+            {
+                var quantity = int.Parse(Console.ReadLine());
+
+                if (!materials.ContainsKey(product))
+                {
+                    materials[product] = 0;
+                }
+                materials[product] += quantity;
+            }
+
+            foreach (var kvp in materials)
+            {
+                Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+            }
+        }
+    }
+}
