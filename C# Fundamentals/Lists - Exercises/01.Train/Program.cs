30fb39175c8988a712b20d12b8664a0a23dbb863
tonsan221 <68417157+tonsan221@users.noreply.github.com>
2020-09-15 13:56:20 +0300

    Add files via upload

---
+++ C# Fundamentals/Lists - Exercises/01.Train/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            int capacity = int.Parse(Console.ReadLine());
+
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] splitInput = input.Split(" ");
+
+                if (splitInput.Length == 1)
+                {
+                    int passengers = int.Parse(splitInput[0]);
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        if (wagons[i] + passengers <= capacity)
+                        {
+                            wagons[i] += passengers;
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    int passengers = int.Parse(splitInput[1]);
+                    wagons.Add(passengers);
+                }
+            }
+            Console.WriteLine(string.Join(" ", wagons));
+        }
+    }
+}
---
+++ C# Fundamentals/Lists - Exercises/02.ChangeList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.ChangeList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                var command = input.Split();
+                if (command[0] == "Delete")
+                {
+                    int num = int.Parse(command[1]);
+                    numbers.RemoveAll(x => x == num);
+                }
+                if (command[0] == "Insert")
+                {
+                    int num = int.Parse(command[1]);
+                    int index = int.Parse(command[2]);
+                    numbers.Insert(index, num);
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
