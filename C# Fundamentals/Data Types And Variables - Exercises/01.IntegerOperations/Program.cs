722c7537668aed153b1d8d2597ae82cec15c0b75
tonsan221 <68417157+tonsan221@users.noreply.github.com>
2020-09-15 13:14:32 +0300

    Add files via upload

---
+++ C# Fundamentals/Data Types And Variables - Exercises/01.IntegerOperations/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01.IntegerOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstNum = int.Parse(Console.ReadLine());
+            var secondNum = int.Parse(Console.ReadLine());
+            var thirdNum = int.Parse(Console.ReadLine());
+            var fourthNum = int.Parse(Console.ReadLine());
+
+            Console.WriteLine((firstNum + secondNum) / thirdNum * fourthNum);
+        }
+    }
+}
