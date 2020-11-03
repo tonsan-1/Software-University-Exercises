8bdf18973ebb55890e6b40dc29d82b1a5cc2bae0
Stoyan Kostadinov - tonsan1 <tonsan221@gmail.com>
2020-11-03 12:54:53 +0200

    rest services and ajax exercises

---
+++ JS-Applications-Course/Rest Services And AJAX/01.BUS-STOP/app.js
@@ -0,0 +1,32 @@
+function getInfo() {
+    let idElement = document.querySelector('#stopId');
+    let busesElement = document.querySelector('#buses');
+    let stopNameElement = document.querySelector('#stopName');
+
+    let validIdNumbers = ['1287', '1308', '1327', '2334'];
+
+    if (validIdNumbers.includes(idElement.value)) {
+        let url = `https://judgetests.firebaseio.com/businfo/${idElement.value}.json`;
+
+        fetch(url)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                let stopName = data.name;
+                let allBuses = data.buses;
+
+                stopNameElement.textContent = stopName;
+                busesElement.innerHTML = '';
+                for (const [bus,time] of Object.entries(allBuses)) {
+                    let liElement = document.createElement('li');
+                    liElement.textContent = `Bus ${bus} in ${time} minutes`;
+
+                    busesElement.appendChild(liElement);
+                }
+            });
+    } else {
+        stopName.textContent = 'Error';
+        busesElement.innerHTML = '';
+    }
+    idElement.value = '';
+}
