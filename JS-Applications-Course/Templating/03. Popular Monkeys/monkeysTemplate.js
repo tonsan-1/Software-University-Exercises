8699c865365ea88cfdd772f38443c378e70bc416
Stoyan Kostadinov - tonsan1 <tonsan221@gmail.com>
2020-11-13 11:05:16 +0200

    templating exercise completed

--- JS-Applications-Course/Templating/03. Popular Monkeys/monkeysTemplate.js
+++ JS-Applications-Course/Templating/03. Popular Monkeys/monkeysTemplate.js
@@ -1,3 +1,32 @@
-$(() => {
-    // TODO
-})
+import monkeys from './monkeys.js';
+
+const elements = {
+    allMonkeys: () => document.querySelector('div.monkeys'),
+};
+
+fetch('./template.hbs')
+    .then((r) => r.text())
+    .then((monkeyTemplateSrc) => {
+        let template = Handlebars.compile(monkeyTemplateSrc);
+        let resultHtml = template({ monkeys });
+        elements.allMonkeys().innerHTML = resultHtml;
+        attachEventListener();
+    })
+    .catch((e) => console.error(e));
+
+function attachEventListener() {
+    elements.allMonkeys().addEventListener('click', (e) => {
+        const { target } = e;
+
+        if (target.nodeName === 'BUTTON' && target.textContent === 'Info') {
+            let pElement = e.target.parentNode.querySelector('p');
+
+            if (pElement.style.display === 'none') {
+                pElement.style.display = 'block';
+            } else {
+                pElement.style.display = 'none';
+            }
+        }
+
+    });
+}
