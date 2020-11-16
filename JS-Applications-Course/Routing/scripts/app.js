728b290c2d57ca2fbf103fa23b7a783ac39d1f3b
Stoyan Kostadinov - tonsan1 <tonsan221@gmail.com>
2020-11-16 14:34:18 +0200

    logout function done

--- JS-Applications-Course/Routing/scripts/app.js
+++ JS-Applications-Course/Routing/scripts/app.js
@@ -3,7 +3,8 @@ const router = Sammy('#main', function () {
     this.use('Handlebars', 'hbs');
 
     // GET
-    this.get('/home', function () {
+    this.get('/home', function (context) {
+        checkIfUserIsSigned(context);
 
         this.loadPartials({
             'header': './templates/common/header.hbs',
@@ -21,6 +22,15 @@ const router = Sammy('#main', function () {
             this.partial('./templates/login/loginPage.hbs');
         });
     });
+    this.get('/logout', function () {
+
+        firebase.auth().signOut()
+            .then((response) => {
+                localStorage.removeItem('userInfo');
+                this.redirect('/login');
+            })
+            .catch((e) => notification('error',e.message));
+    });
     this.get('/register', function () {
         this.loadPartials({
             'header': './templates/common/header.hbs',
@@ -30,7 +40,9 @@ const router = Sammy('#main', function () {
             this.partial('./templates/register/registerPage.hbs');
         });
     });
-    this.get('/about', function () {
+    this.get('/about', function (context) {
+        checkIfUserIsSigned(context);
+
         this.loadPartials({
             'header': './templates/common/header.hbs',
             'footer': './templates/common/footer.hbs',
@@ -40,8 +52,8 @@ const router = Sammy('#main', function () {
     });
 
     // POST
-    this.post('/register', function(context){
-        const { email , password, repeatPassword} = context.params;
+    this.post('/register', function (context) {
+        const { email, password, repeatPassword } = context.params;
 
         if (password !== repeatPassword) {
             notification('error', 'Passwords must match!')
@@ -53,20 +65,41 @@ const router = Sammy('#main', function () {
                 notification('info', 'Registration was successful!');
                 this.redirect('/login');
             })
-            .catch((e) => console.error(e));
+            .catch((e) => notification('error', e.message));
     });
-    
+    this.post('/login', function (context) {
+        const { email, password } = context.params;
+
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(({ user: { uid, email } }) => {
+                notification('info', 'Login was successful!');
+                localStorage
+                    .setItem('userInfo', JSON.stringify({ uid, email }));
+                context.redirect('/home');
+            })
+            .catch((e) => notification('error', e.message));
+    });
 });
 
 (() => {
     router.run('/home');
 })()
 
-function notification(type,message){
+function notification(type, message) {
     let element = document.querySelector(`#${type}Box`); // type is either error or info
     element.textContent = message;
     element.style.display = 'block';
-    setInterval(function (){
+    setInterval(function () {
         element.style.display = 'none';
-    },2000);
+    }, 4000);
+}
+function checkIfUserIsSigned(context) {
+    const userInfo = localStorage.getItem('userInfo');
+
+    if (userInfo) {
+        const { uid, email } = JSON.parse(userInfo);
+
+        context.loggedIn = true;
+        context.username = email.split('@')[0];
+    }
 }
