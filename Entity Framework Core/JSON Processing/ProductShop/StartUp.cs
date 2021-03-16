5e0e3ef2069aeba8c3ccfa557ef58b8043473ddd
unknown <68417157+tonsan221@users.noreply.github.com>
2021-03-16 15:45:48 +0200

    .

--- Entity Framework Core/JSON Processing/ProductShop/StartUp.cs
+++ Entity Framework Core/JSON Processing/ProductShop/StartUp.cs
@@ -7,6 +7,7 @@ using ProductShop.DTO;
 using ProductShop.Models;
 using System.Collections.Generic;
 using AutoMapper;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProductShop
 {
@@ -17,21 +18,120 @@ namespace ProductShop
         {
             var productShopContext = new ProductShopContext();
 
-            productShopContext.Database.EnsureDeleted();
-            productShopContext.Database.EnsureCreated();
+            //productShopContext.Database.EnsureDeleted();
+            //productShopContext.Database.EnsureCreated();
 
-            var inputUsersJson = File.ReadAllText("../../../Datasets/users.json");
-            var inputProductsJson = File.ReadAllText("../../../Datasets/products.json");
-            var inputCategoriesJson = File.ReadAllText("../../../Datasets/categories.json");
-            var inputProductsCategoriesJson = File.ReadAllText("../../../Datasets/categories-products.json");
+            //var inputUsersJson = File.ReadAllText("../../../Datasets/users.json");
+            //var inputProductsJson = File.ReadAllText("../../../Datasets/products.json");
+            //var inputCategoriesJson = File.ReadAllText("../../../Datasets/categories.json");
+            //var inputProductsCategoriesJson = File.ReadAllText("../../../Datasets/categories-products.json");
 
-            ImportUsers(productShopContext, inputUsersJson);
-            ImportProducts(productShopContext, inputProductsJson);
-            ImportCategories(productShopContext, inputCategoriesJson);
+            //ImportUsers(productShopContext, inputUsersJson);
+            //ImportProducts(productShopContext, inputProductsJson);
+            //ImportCategories(productShopContext, inputCategoriesJson);
+            //ImportCategoryProducts(productShopContext, inputProductsCategoriesJson);
 
-            Console.WriteLine(ImportCategoryProducts(productShopContext, inputProductsCategoriesJson));
+            Console.WriteLine(GetUsersWithProducts(productShopContext));
         }
+        public static string GetUsersWithProducts(ProductShopContext context)
+        {
+            var users = context.Users
+                .Include(x => x.ProductsSold)
+                .ToList()
+                .Where(x => x.ProductsSold.Count > 0)
+                .Select(y => new
+                {
+                    firstName = y.FirstName,
+                    lastName = y.LastName,
+                    age = y.Age,
+                    soldProducts = new
+                    {
+                        count = y.ProductsSold.Count(),
+                        products = y.ProductsSold.Select(p => new
+                        {
+                            name = p.Name,
+                            price = p.Price
+                        })
+                    }
+                })
+                .OrderByDescending(x => x.soldProducts.count)
+                .ToList();
 
+            var resultObj = new
+            {
+                usersCount = users.Count(),
+                users = users
+            };
+
+            var jsonSerializerSettings = new JsonSerializerSettings
+            {
+                NullValueHandling = NullValueHandling.Ignore
+            };
+
+            var json = JsonConvert.SerializeObject(resultObj, Formatting.Indented, jsonSerializerSettings);
+
+            return json;
+        }
+        public static string GetCategoriesByProductsCount(ProductShopContext context)
+        {
+            var categories = context.Categories
+                .Select(x => new
+                {
+                    category = x.Name,
+                    productsCount = x.CategoryProducts.Count,
+                    averagePrice = x.CategoryProducts.Average(y => y.Product.Price).ToString("F2"),
+                    totalRevenue = x.CategoryProducts.Sum(y => y.Product.Price).ToString("F2")
+                })
+                .OrderByDescending(x => x.productsCount)
+                .ToList();
+
+            var json = JsonConvert.SerializeObject(categories,Formatting.Indented);
+
+            return json;
+        }
+        public static string GetSoldProducts(ProductShopContext context)
+        {
+            var soldProducts = context.Users
+                .Where(x => x.ProductsSold.Any(y => y.BuyerId != null))
+                .Select(x => new
+                {
+                    firstName = x.FirstName,
+                    lastName = x.LastName,
+                    age = x.Age,
+                    soldProducts = x.ProductsSold.Where(b => b.BuyerId != null)
+                                                 .Select(y => new
+                    {
+                        name = y.Name,
+                        price = y.Price,
+                        buyerFirstName = y.Buyer.FirstName,
+                        buyerLastName = y.Buyer.LastName
+                    })
+                })
+                .OrderBy(x => x.lastName)
+                .ThenBy(x => x.firstName)
+                .ToList();
+
+            var json = JsonConvert.SerializeObject(soldProducts, Formatting.Indented);
+
+            return json;
+        }
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            var products = context.Products
+                .Where(x => x.Price >= 500 && x.Price <= 1000)
+                .Select(x => new
+                {
+                    name = x.Name,
+                    price = x.Price,
+                    seller = x.Seller.FirstName + ' ' + x.Seller.LastName
+                })
+                .OrderBy(x => x.price)
+                .ToList();
+
+            var json = JsonConvert.SerializeObject(products,Formatting.Indented);
+
+            return json;
+        }
         public static string ImportCategoryProducts(ProductShopContext context, string inputJson)
         {
             InitializeAutoMapper();
